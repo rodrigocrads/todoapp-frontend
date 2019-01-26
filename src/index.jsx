e4fc58393108c6a04f03052393ad7c0c29de675a
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2019-01-26 11:48:20 -0200

    feat(ReduxDevTools): Impl redux dev tools pluggin

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import App from './main/app'
-import reducers from './main/reducers'
+import App from './main/app';
+import reducers from './main/reducers';
 
-const store = createStore(reducers)
+const devTools = window.__REDUX_DEVTOLLS_EXTENSION__ 
+    && window.__REDUX_DEVTOLLS_EXTENSION__();
+
+const store = createStore(reducers, devTools);
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('app')
-)
+);
